725adaf8a67d03e8b9456f8210bf1f4a7b27cd93
wh136 <136856026@qq.com>
2018-04-23 10:53:27 +0800

    Add baidu union page view

---
+++ baidu_union.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?25dd518910c914e2f273428933c57baf";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
+
